daf6f8edc8cbd2076fc8eb38128b1d5f99342551
Shai Goldberg-Kellman <sgoldbergkellman@gmail.com>
2018-04-16 23:47:38 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'snickers']
 function addElementToBeginningOfArray (array, element) {
-  return [element ...array]
+  [element ...array]
 }
